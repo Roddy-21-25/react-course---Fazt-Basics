7b21c376a69777d5e269af614b64678fa099b118
Roddy-Rafael-Tejeda-Rosario-21-25 <128174391+Roddy-21-25@users.noreply.github.com>
2023-11-01 00:28:06 -0400

    first commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,97 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React, { useState } from "react";
+import ReactDOM from "react-dom/client";
+import { Greeting, UserCard } from "./Greeting";
+import Product from "./Product";
+import { Button } from "./Button";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import { TaskCard } from "./Task";
+
+import { Saludar } from "./Saludar";
+
+import { Post } from "./Posts";
+
+// Esto es el Dom Virtual de React
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+// const handleChange = (e) => {
+//   console.log(e.target.value);
+// }
+
+const user = [
+  {
+    id: 1,
+    name: "Ryan ray",
+    image: "https://robohash.org/user1",
+  },
+  {
+    id: 2,
+    name: "Joe",
+    image: "https://robohash.org/user2",
+  },
+  {
+    id: 3,
+    name: "Roddy",
+    image: "https://robohash.org/roddy",
+  },
+];
+
+function Mensaje() {
+  const [mensaje, setMensaje] = useState('');
+
+  return (
+    <div>
+      <input onChange={e => setMensaje(e.target.value)} />
+      <button onClick={() => {
+        alert('El mensaje es: ' + mensaje)
+      }}>
+        Save
+      </button>
+    </div>
+  );
+}
+
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+  <>
+    <h1>Counter: 0</h1>
+    <button>Sumar</button>
+    <hr/>
+    <Mensaje />
+    {/* {user.map((user, i) => {
+      return(
+        <div key={i}>
+          <h1>{user.name}</h1>
+          <img src={user.image}/>
+        </div>
+      )
+    })} */}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+    {/* <Post/> */}
+
+    {/* <Button text="Hola mundo" name="Roddy"/>
+
+  <input id="hola" onChange={handleChange} /> */}
+
+    {/* <Saludar/> */}
+
+    {/* <TaskCard ready={true}/>
+  <TaskCard ready={false}/> */}
+
+    {/* <Button text="Click Me" name="Usuario"/>
+  <Button text="Pay"/>
+  <Button text="Go To"/>
+  <Button text={3000}/> */}
+
+    {/* <Greeting title="Hola mundo" name="React" />
+    <Product />
+    <UserCard
+      name="Roddy R. T. R."
+      amount={3000}
+      married={true}
+      points={[99, 33.3, 22.2]}
+      address={{ street: "123 Main Street", city: "New York" }}
+      greet={function () {
+        alert("alerta");
+      }}
+    /> */}
+  </>
+);
